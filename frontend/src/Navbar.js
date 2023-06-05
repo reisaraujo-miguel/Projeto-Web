f72fbb81854d0dfdd9308d318a68117b999d80b7
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2023-06-05 12:50:24 -0300

    Renaming attributes
    JSX (React) uses camelCase for html attributes, so it was throwing warings inside the browser console.

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -3,50 +3,50 @@ import './Navbar.css';
 
 const NavBar = () => {
     return (
-        <div class='mb-5'>
-            <nav class='navbar navbar-dark fixed-top' style={{ 'background-color': '#6200EE' }}>
-                <div class='container-fluid'>
-                    <button class='navbar-toggler' type='button' data-bs-toggle='offcanvas'
+        <div className='mb-5'>
+            <nav className='navbar navbar-dark fixed-top' style={{ 'backgroundColor': '#6200EE' }}>
+                <div className='container-fluid'>
+                    <button className='navbar-toggler' type='button' data-bs-toggle='offcanvas'
                         data-bs-target='#offcanvasDarkNavbar' aria-controls='offcanvasDarkNavbar'
                         aria-label='Toggle navigation'>
-                        <span class='navbar-toggler-icon fs-5'></span>
+                        <span className='navbar-toggler-icon fs-5'></span>
                     </button>
-                    <a class='navbar-brand fw-bold' style={{'font-size': '180%', 'font-family': 'Montagu Slab'}} href='/#'> Silicon Road </a>
-                    <form class='d-flex' role='search'>
-                        <input class='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
-                            <button class='btn btn-outline-light' type='submit'>Search</button>
+                    <a className='navbar-brand fw-bold' style={{'fontSize': '180%', 'fontFamily': 'Montagu Slab'}} href='/#'> Silicon Road </a>
+                    <form className='d-flex' role='search'>
+                        <input className='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
+                            <button className='btn btn-outline-light' type='submit'>Search</button>
                     </form>
-                    <div class='offcanvas offcanvas-start text-bg-dark' tabindex='-1' id='offcanvasDarkNavbar'
+                    <div className='offcanvas offcanvas-start text-bg-dark' tabIndex='-1' id='offcanvasDarkNavbar'
                         aria-labelledby='offcanvasDarkNavbarLabel'>
-                        <div class='offcanvas-header'>
-                            <h5 class='offcanvas-title' id='offcanvasDarkNavbarLabel'>Dark offcanvas</h5>
-                            <button type='button' class='btn-close btn-close-white' data-bs-dismiss='offcanvas'
+                        <div className='offcanvas-header'>
+                            <h5 className='offcanvas-title' id='offcanvasDarkNavbarLabel'>Dark offcanvas</h5>
+                            <button type='button' className='btn-close btn-close-white' data-bs-dismiss='offcanvas'
                                 aria-label='Close'></button>
                         </div>
-                        <div class='offcanvas-body'>
-                            <ul class='navbar-nav justify-content-start flex-grow-1 pe-3'>
-                                <li class='nav-item'>
-                                    <a class='nav-link active' aria-current='page' href='/#'>Home</a>
+                        <div className='offcanvas-body'>
+                            <ul className='navbar-nav justify-content-start flex-grow-1 pe-3'>
+                                <li className='nav-item'>
+                                    <a className='nav-link active' aria-current='page' href='/#'>Home</a>
                                 </li>
-                                <li class='nav-item'>
-                                    <a class='nav-link' href='/#'>Link</a>
+                                <li className='nav-item'>
+                                    <a className='nav-link' href='/#'>Link</a>
                                 </li>
-                                <li class='nav-item dropdown'>
-                                    <a class='nav-link dropdown-toggle' href='/#' role='button' data-bs-toggle='dropdown'
+                                <li className='nav-item dropdown'>
+                                    <a className='nav-link dropdown-toggle' href='/#' role='button' data-bs-toggle='dropdown'
                                         aria-expanded='false'> Dropdown </a>
-                                    <ul class='dropdown-menu dropdown-menu-dark'>
-                                        <li><a class='dropdown-item' href='/#'>Action</a></li>
-                                        <li><a class='dropdown-item' href='/#'>Another action</a></li>
+                                    <ul className='dropdown-menu dropdown-menu-dark'>
+                                        <li><a className='dropdown-item' href='/#'>Action</a></li>
+                                        <li><a className='dropdown-item' href='/#'>Another action</a></li>
                                         <li>
-                                            <hr class='dropdown-divider' />
+                                            <hr className='dropdown-divider' />
                                         </li>
-                                        <li><a class='dropdown-item' href='/#'>Something else here</a></li>
+                                        <li><a className='dropdown-item' href='/#'>Something else here</a></li>
                                     </ul>
                                 </li>
                             </ul>
-                            <form class='d-flex mt-3' role='search'>
-                                <input class='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
-                                    <button class='btn btn-success' type='submit'>Search</button>
+                            <form className='d-flex mt-3' role='search'>
+                                <input className='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
+                                    <button className='btn btn-success' type='submit'>Search</button>
                             </form>
                         </div>
                     </div>
