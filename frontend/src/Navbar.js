154b4206d90d86b7587ecc912b1dacb7461ee04b
fernandoclbf <fernandoclbf@usp.br>
2023-06-12 00:03:39 -0300

    Creation of links inside the Offcanvas Menu.
    Creation of links for other pages inside the Offcanvas Menu.

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -19,35 +19,23 @@ const NavBar = () => {
                     <div className='offcanvas offcanvas-start text-bg-dark' tabIndex='-1' id='offcanvasDarkNavbar'
                         aria-labelledby='offcanvasDarkNavbarLabel'>
                         <div className='offcanvas-header'>
-                            <h5 className='offcanvas-title' id='offcanvasDarkNavbarLabel'>Dark offcanvas</h5>
+                            <h5 className='offcanvas-title' id='offcanvasDarkNavbarLabel'>Main Menu</h5>
                             <button type='button' className='btn-close btn-close-white' data-bs-dismiss='offcanvas'
                                 aria-label='Close'></button>
                         </div>
                         <div className='offcanvas-body'>
                             <ul className='navbar-nav justify-content-start flex-grow-1 pe-3'>
                                 <li className='nav-item'>
-                                    <a className='nav-link active' aria-current='page' href='/#'>Home</a>
+                                    <a className='nav-link active' aria-current='page' href='/'>Home</a>
                                 </li>
                                 <li className='nav-item'>
-                                    <a className='nav-link' href='/#'>Link</a>
+                                    <a className='nav-link' href='/checkout'>Checkout</a>
                                 </li>
-                                <li className='nav-item dropdown'>
-                                    <a className='nav-link dropdown-toggle' href='/#' role='button' data-bs-toggle='dropdown'
-                                        aria-expanded='false'> Dropdown </a>
-                                    <ul className='dropdown-menu dropdown-menu-dark'>
-                                        <li><a className='dropdown-item' href='/#'>Action</a></li>
-                                        <li><a className='dropdown-item' href='/#'>Another action</a></li>
-                                        <li>
-                                            <hr className='dropdown-divider' />
-                                        </li>
-                                        <li><a className='dropdown-item' href='/#'>Something else here</a></li>
-                                    </ul>
+                                <li className='nav-item'>
+                                    <a className='nav-link' href='/product'>Product Page</a>
                                 </li>
+                                
                             </ul>
-                            <form className='d-flex mt-3' role='search'>
-                                <input className='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
-                                    <button className='btn btn-success' type='submit'>Search</button>
-                            </form>
                         </div>
                     </div>
                 </div>
