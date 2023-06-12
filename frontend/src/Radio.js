881cc046c10eab2d777db2fda4506b0d88f80d65
chonkachu <chrissimascgr@gmail.com>
2023-06-12 01:16:45 -0300

    adding radio button component

---
+++ frontend/src/Radio.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Radio.css'
+
+const Radio = ({ value, onClick, backgroundColor }) => {
+
+  const inputStyle = {
+    backgroundColor: backgroundColor
+  };
+
+  return (
+    <div className="radio-button-wrapper" style={inputStyle}>
+    <input
+      type="radio"
+      onClick={onClick}
+    />
+    <label>{value}</label>
+    </div>
+  );
+};
+
+export default Radio;
