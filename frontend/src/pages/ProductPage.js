e29c6ed06b085a1c938a7c53c9cbed86e048baad
fernandoclbf <fernandoclbf@usp.br>
2023-06-12 00:56:32 -0300

    Updating the Product Page
    Updating the Product Page, zé da manga.

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,6 +1,9 @@
 // pages/ProductPage.js
 
 import React from "react";
+import ImgPreviewStack from '../ImageSelection';
+import RatingWidget from "../RatingWidget";
+import CustomButton from '../Buttons.js';
 
 const ProductPage = () => (
   <div>
@@ -11,6 +14,13 @@ const ProductPage = () => (
       et ultrices posuere cubilia curae; Duis consequat nulla ac ex consequat,
       in efficitur arcu congue. Nam fermentum commodo egestas.
     </p>
+
+    <ImgPreviewStack test={true}/>
+    <RatingWidget/>
+    <CustomButton button_size='small-button' alt='plus icon' image='./img/Buttons/plus-icon.svg' text='ADD TO CART' />
+    <CustomButton text='BUY NOW' />
+    
+
   </div>
 );
 
