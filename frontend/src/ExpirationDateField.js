8724394bb3856fbf323398aca5870cdbedd43850
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2023-06-13 01:14:12 -0300

    Finished the PaymentMethod component
    Decided to only offer credit card payment, as that is what was required
    by the client (eg. Proffessor).
    
    The `setJson` argument should be a function  that saves the content of
    the card information. The content is returned in Json format so it can
    be saved in the backend and verifyied later.

--- frontend/src/ExpirationDateField.js
+++ frontend/src/ExpirationDateField.js
@@ -1,71 +1,75 @@
 import React, { useState } from 'react';
 import './ExpirationDateField.css';
 
-const ExpirationDateField = () => {
-  const [expirationDate, setExpirationDate] = useState('');
-  const [isValid, setIsValid] = useState(true);
+const ExpirationDateField = ({ setData }) => {
+    const [expirationDate, setExpirationDate] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
-  const handleExpirationDateChange = (event) => {
-    let value = event.target.value;
+    const handleExpirationDateChange = (event) => {
+        let value = event.target.value;
 
-    // Remove any non-digit and non-slash characters
-    value = value.replace(/[^0-9/]/g, '');
+        // Remove any non-digit and non-slash characters
+        value = value.replace(/[^0-9/]/g, '');
 
-    // Insert '/' after the month (2 characters) if missing
-    if (value.length > 2 && value.indexOf('/') === -1) {
-      value = value.slice(0, 2) + '/' + value.slice(2);
-    }
+        // Insert '/' after the month (2 characters) if missing
+        if (value.length > 2 && value.indexOf('/') === -1) {
+            value = value.slice(0, 2) + '/' + value.slice(2);
+        }
 
-    setExpirationDate(value);
-    setIsValid(true); // Reset validity on change
-  };
+        setExpirationDate(value);
+        setIsValid(true); // Reset validity on change
+    };
 
-  const verifyExpirationDate = () => {
-    const currentDate = new Date();
-    const [month, year] = expirationDate.split('/');
+    const verifyExpirationDate = () => {
+        const currentDate = new Date();
+        const [month, year] = expirationDate.split('/');
 
-    if (
-      expirationDate.length !== 5 ||
-      isNaN(month) ||
-      isNaN(year) ||
-      Number(month) < 1 ||
-      Number(month) > 12
-    ) {
-      setIsValid(false);
-      return;
-    }
+        if (
+            expirationDate.length !== 5 ||
+            isNaN(month) ||
+            isNaN(year) ||
+            Number(month) < 1 ||
+            Number(month) > 12
+        ) {
+            setIsValid(false);
+            return;
+        }
 
-    const expiration = new Date(Number(`20${year}`), Number(month) - 1);
-    const isValidDate = expiration > currentDate;
+        const expiration = new Date(Number(`20${year}`), Number(month) - 1);
+        const isValidDate = expiration > currentDate;
 
-    setIsValid(isValidDate);
-  };
+        if (isValidDate) {
+            setData(expiration);
+        }
 
-  const handleBlur = () => {
-    verifyExpirationDate();
-  };
+        setIsValid(isValidDate);
+    };
 
-  return (
-    <div>
-      <label>
-        Expiration Date:
+    const handleBlur = () => {
+        verifyExpirationDate();
+    };
+
+    return (
         <div>
-          <input
-            type="text"
-            value={expirationDate}
-            onChange={handleExpirationDateChange}
-            onBlur={handleBlur}
-            placeholder="MM/YY"
-            maxLength="5"
-            className={`input-field ${isValid ? '' : 'invalid'}`}
-          />
+            <label>
+                Expiration Date:
+                <div>
+                    <input
+                        type="text"
+                        value={expirationDate}
+                        onChange={handleExpirationDateChange}
+                        onBlur={handleBlur}
+                        placeholder="MM/YY"
+                        maxLength="5"
+                        className={`input-field ${isValid ? '' : 'invalid'}`}
+                    />
+                </div>
+                {!isValid && (
+                    <div className="error-message">Invalid or expired date!</div>
+                )}
+            </label>
         </div>
-        {!isValid && (
-            <div className="error-message">Invalid or expired date!</div>
-        )}
-      </label>
-    </div>
-  );
+    );
 };
 
 export default ExpirationDateField;
