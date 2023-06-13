74c7e68ea87f687a05a3e62434f0bc6420bccbe6
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2023-06-13 15:20:28 -0300

    Pulled page outside <p></p>

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,39 +11,39 @@ import Carousel from '../Carousel';
 
 
 const Home = () => (
-  <div>
-    <h1 className="title is-1">Esta é a página principal</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
-      risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
-      nulla interdum justo. Pellentesque dignissim, sapien et congue rutrum,
-      lorem tortor dapibus turpis, sit amet vestibulum eros mi et odio.
+    <div>
+        <h1 className="title is-1">Esta é a página principal</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
+            risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
+            nulla interdum justo. Pellentesque dignissim, sapien et congue rutrum,
+            lorem tortor dapibus turpis, sit amet vestibulum eros mi et odio.
+        </p>
 
-      <img src={logo} className="App-logo" alt="logo" />
-                <CustomButton button_size='small-button' alt='plus icon' image='./img/Buttons/plus-icon.svg' text='ICON + TEXT' />
-                <CustomButton text='ONLY TEXT' />
-                <CustomButton image='./img/Buttons/plus-icon.svg' alt="plus icon" />
-                <CustomButton image='./img/Buttons/plus-icon.svg' bg='no-bg' />
-                <ExpirationDateField />
-                <RatingWidget />
-                <SearchBarAdmin />
-                <ImgPreviewStack test={true} />
-                <InputBox/>
-                <Shipment/>
-                <Carousel/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-    </p>
-  </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <CustomButton button_size='small-button' alt='plus icon' image='./img/Buttons/plus-icon.svg' text='ICON + TEXT' />
+        <CustomButton text='ONLY TEXT' />
+        <CustomButton image='./img/Buttons/plus-icon.svg' alt="plus icon" />
+        <CustomButton image='./img/Buttons/plus-icon.svg' bg='no-bg' />
+        <ExpirationDateField />
+        <RatingWidget />
+        <SearchBarAdmin />
+        <ImgPreviewStack test={true} />
+        <InputBox />
+        <Shipment />
+        <Carousel />
+        <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Learn React
+        </a>
+    </div>
 );
 
 export default Home;
