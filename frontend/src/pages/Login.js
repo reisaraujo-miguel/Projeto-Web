c1a5c2e214010f5db3e0be3fc7400b06750c8d5f
fernandoclbf <fernandoclbf@usp.br>
2023-06-13 23:05:29 -0300

    Finishing the Login Page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -8,14 +8,26 @@ import Password from '../img/icons/password.png';
 import CustomButton from '../Buttons';
 
 const Login = () => (
+  
   <div className="loginDiv">
-    <h1 style={{marginBottom:"50px"}}>Login</h1>
+    <h1 style={{ marginBottom: "50px" }}>Login</h1>
     <InputBox image={UserImage} inputType={"text"} placeholder={"Username"} />
     <br style={{ marginBottom: '30px' }}></br>
     <InputBox image={Password} inputType={"password"} placeholder={"Password"} />
-    <br style={{ marginBottom: '30px' }}></br>
-    <CustomButton text={"Log in"}/>
+
+    <div className="loginOptions">
+      <input type="checkbox" id="remember-me" name="remember-me" defaultChecked={true}></input>
+      <label for="remember-me" className="rememberMe">Remember me</label>
+      <label className="forgotPassword"><a href="none">Forgot password?</a></label>
+    </div>
+    <br style={{ marginBottom: '10px' }}></br>
+    <CustomButton text={"Log in"} />
+    <br style={{ marginBottom: '20px' }}></br>
+    <label className="createAccount">Don’t have an account yet? <a href="signup">Sign up</a></label>
+
   </div>
+
+
 );
 
 export default Login;
